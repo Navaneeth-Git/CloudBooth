2868c95b80c3f3cb4b907683ec9b8c7ac4f9cb9a
QuackityDuck <navaneethnandakumar12@gmail.com>
2025-06-04 01:28:01 +0530

    Initial Commit

---
+++ CloudBooth/CloudBoothApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudBoothApp.swift
+//  CloudBooth
+//
+//  Created by Navaneeth on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct CloudBoothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
